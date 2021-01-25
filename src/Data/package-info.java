4d9ba98bf958e0f9500703521fe4b9a0bb2d5d19
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-01-25 01:54:38 +0100

    final commit - package info

--- src/Data/package-info.java
+++ src/Data/package-info.java
@@ -1,7 +1,6 @@
 /**
  * Package includes classes necessary to calculate calendar that is common part of 2 input calendars.
  * Algorithm works in O(N) time complexity.
- *
  * @author Jakub Zieliński
  * @version final-version
  */package Data;
--- src/Server/package-info.java
+++ src/Server/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Package includes classes  to create Server app that listens for connections from client apps, deploys
- * handlers that process request and send back final product to clients.*
+ * handlers that process request and send back final product to clients.
  * Build according  to MVC project pattern.
  * @author Jakub Zieliński
  * @version final-version
