6c1e0f47155261aa89b40f5c3dfd42df84fa1bcc
Jakub Zieliński <j.zielinski@stud.elka.pw.edu.pl>
2021-01-25 01:53:54 +0100

    final commit - package info

---
+++ src/Client/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package includes classes  to create client app that takes input calendar from client, connects to designated server app
+ * and retrieves and shows final calendar to end user.
+ *
+ * Build according  to MVC project pattern.
+ * @author Jakub Zieliński
+ * @version final-version
+ */
+
+package Client;
---
+++ src/Server/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package includes classes  to create Server app that listens for connections from client apps, deploys
+ * handlers that process request and send back final product to clients.*
+ * Build according  to MVC project pattern.
+ * @author Jakub Zieliński
+ * @version final-version
+ */
+
+package Server;
